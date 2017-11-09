ea5bfa6b2b7faa01a8e5034643228e16401ac2ff
takata-daiki <32923384+takata-daiki@users.noreply.github.com>
2017-11-09 17:14:18 +0900

    Add files via upload

--- components/bB.vhd
+++ components/bB.vhd
@@ -6,19 +6,18 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_signed.all;
 
 entity bB is
-  port(GR, PR, MAR, MDR : in std_logic_vector(15 downto 0)
-       addr   : in std_logic_vector(7 downto 0)
-       s_ctl  : in std_logic_vector(4 downto 0)
+  port(GR, PR, MAR, MDR : in std_logic_vector(15 downto 0);
+       addr   : in std_logic_vector(7 downto 0);
+       s_ctl  : in std_logic_vector(4 downto 0);
        busB    : out std_logic_vector(15 downto 0));
 end bB;
 
 architecture BEHAVIOR of bB is
 begin
-  case s_ctl is
-    when "10000" => busB <= GR;
-    when "01000" => busB <= PR;
-    when "00100" => busB <= MAR;
-    when "00010" => busB <= MDR;
-    when "00001" => busB <= "00000000" & addr;
-  end case;
+  busB <= GR  when s_ctl = "10000"
+    else  PR  when s_ctl = "01000"
+    else  MAR when s_ctl = "00100"
+    else  MDR when s_ctl = "00010"
+    else  "00000000" & addr when s_ctl = "00001"
+    else  "XXXXXXXXXXXXXXXX";
 end BEHAVIOR;

f2986fb9dc34f8336f905e1018c8314794526eb4
takata-daiki <32923384+takata-daiki@users.noreply.github.com>
2017-11-10 13:28:25 +0900

    Update bB.vhd
    変数名の変更

--- components/bB.vhd
+++ components/bB.vhd
@@ -6,18 +6,18 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_signed.all;
 
 entity bB is
-  port(GR, PR, MAR, MDR : in std_logic_vector(15 downto 0);
-       addr   : in std_logic_vector(7 downto 0);
-       s_ctl  : in std_logic_vector(4 downto 0);
-       busB   : out std_logic_vector(15 downto 0));
+  port(S_GRB, S_PR_F, S_MAR_F, S_MDR_F : in std_logic_vector(15 downto 0);
+       addr     : in std_logic_vector(7 downto 0);
+       S_s_ctl  : in std_logic_vector(4 downto 0);
+       S_BUS_B  : out std_logic_vector(15 downto 0));
 end bB;
 
 architecture BEHAVIOR of bB is
 begin
-  busB <= GR  when s_ctl = "10000"
-    else  PR  when s_ctl = "01000"
-    else  MAR when s_ctl = "00100"
-    else  MDR when s_ctl = "00010"
-    else  "00000000" & addr when s_ctl = "00001"
-    else  "XXXXXXXXXXXXXXXX";
+  S_BUS_B <=  S_GRB   when S_s_ctl = "10000"
+    else      S_PR_F  when S_s_ctl = "01000"
+    else      S_MAR_F when S_s_ctl = "00100"
+    else      S_MDR_F when S_s_ctl = "00010"
+    else      "00000000" & S_addr when S_s_ctl = "00001"
+    else      "XXXXXXXXXXXXXXXX";
 end BEHAVIOR;

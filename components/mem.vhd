fa1da91a01afd4d14678f48ca79f3cf1b542ab9c
takata-daiki <32923384+takata-daiki@users.noreply.github.com>
2017-11-27 17:43:23 +0900

    Add files via upload

--- components/mem.vhd
+++ components/mem.vhd
@@ -1,9 +1,12 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
+use ieee.std_logic_textio.all;
+library std;
+use std.textio.all;
 
 entity mem is
-  port(clk, read, write, S_MEMlat : in std_logic;
+  port(clk, read, write : in std_logic;
        S_MAR_F : in std_logic_vector(7 downto 0);
        S_MDR_F : in std_logic_vector(15 downto 0);
        data : out std_logic_vector(15 downto 0));
@@ -12,8 +15,20 @@ end mem;
 architecture BEHAVIOR of mem is
 
 subtype RAM_WORD is std_logic_vector(15 downto 0);
-type RAM_ARRAY is array (0 to 255) of RAM_WORD;
-signal RAM_DATA : RAM_ARRAY;
+type RAM_TYPE is array (0 to 255) of RAM_WORD;
+impure function init_ram_file(RAM_FILE_NAME : in string) return RAM_TYPE is
+file RAM_FILE : TEXT is in RAM_FILE_NAME;
+variable RAM_FILE_LINE : line;
+variable RAM_DIN : RAM_TYPE;
+begin
+  for I in RAM_TYPE'range loop
+    readline(RAM_FILE, RAM_FILE_LINE);
+    hread(RAM_FILE_LINE, RAM_DIN(I));
+  end loop;
+  return RAM_DIN;
+end function;
+
+signal RAM_DATA : RAM_TYPE := init_ram_file("mem.txt");
 signal addr : std_logic_vector(7 downto 0);
 
 begin

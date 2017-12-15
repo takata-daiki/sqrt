2e4e5a3e09e13aae1672dbb63054b23c701c9736
takata-daiki <32923384+takata-daiki@users.noreply.github.com>
2017-12-15 13:45:45 +0900

    テストベンチをわかりやすくした

--- fpga/tb_sqrt.vhd
+++ fpga/tb_sqrt.vhd
@@ -1,38 +1,12 @@
--- Copyright (C) 1991-2013 Altera Corporation
--- Your use of Altera Corporation's design tools, logic functions 
--- and other software and tools, and its AMPP partner logic 
--- functions, and any output files from any of the foregoing 
--- (including device programming or simulation files), and any 
--- associated documentation or information are expressly subject 
--- to the terms and conditions of the Altera Program License 
--- Subscription Agreement, Altera MegaCore Function License 
--- Agreement, or other applicable license agreement, including, 
--- without limitation, that your use is for the sole purpose of 
--- programming logic devices manufactured by Altera and sold by 
--- Altera or its authorized distributors.  Please refer to the 
--- applicable agreement for further details.
-
--- ***************************************************************************
--- This file contains a Vhdl test bench template that is freely editable to   
--- suit user's needs .Comments are provided in each section to help the user  
--- fill out necessary details.                                                
--- ***************************************************************************
--- Generated on "12/12/2017 22:59:59"
-                                                            
--- Vhdl Test Bench template for design  :  sqrt
--- 
--- Simulation tool : ModelSim-Altera (VHDL)
--- 
-
-LIBRARY ieee;                                               
-USE ieee.std_logic_1164.all;                                
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
 USE ieee.std_logic_unsigned.all;
 
 ENTITY tb_sqrt IS
 END tb_sqrt;
 ARCHITECTURE sqrt_arch OF tb_sqrt IS
--- constants                                                 
--- signals                                                   
+-- constants
+-- signals
 SIGNAL btn : STD_LOGIC_VECTOR(2 DOWNTO 0);
 SIGNAL hex0 : STD_LOGIC_VECTOR(7 DOWNTO 0);
 SIGNAL hex1 : STD_LOGIC_VECTOR(7 DOWNTO 0);
@@ -56,7 +30,7 @@ END COMPONENT;
 BEGIN
 	i1 : sqrt
 	PORT MAP (
--- list connections between master ports and signals
+  -- list connections between master ports and signals
 	btn => btn,
 	hex0 => hex0,
 	hex1 => hex1,
@@ -66,44 +40,48 @@ BEGIN
 	--pulse => pulse,
 	sw => sw
 	);
-init : PROCESS                                               
--- variable declarations                                     
-BEGIN                                                        
-        -- code that executes only once                      
-WAIT;                                                       
-END PROCESS init;                                           
-always : PROCESS                                              
--- optional sensitivity list                                  
--- (        )                                                 
--- variable declarations                                      
-BEGIN                                                         
-        -- code executes for every event on sensitivity list 
+init : PROCESS
+-- variable declarations
+BEGIN
+  -- code that executes only once
+WAIT;
+END PROCESS init;
+always : PROCESS
+-- optional sensitivity list
+-- (        )
+-- variable declarations
+BEGIN
+  -- code executes for every event on sensitivity list
+  -- wait for about 3 ms;
+
+  -- ans = sqrt("c350") = X"df.9b" (preset)
+  -- hex[0, 1, 2, 3] = [83, 98, 0e, a1]
   btn <= "111";
   sw <= "0000000000";
+  wait for 1 ms;
 
-  wait for 70000 ns;
-	btn <= "110";
+  -- ans = sqrt(X"fe00") = X"fe.ff"
+  -- hex[0, 1, 2, 3] = [8e, 8e, 06, 8e]
+  btn <= "110";   -- set sw <= X"00", pushing btn(0)
 	sw(7 downto 0) <= X"00";
-	
   wait for 50 ns;
-	btn <= "101";
+  btn <= "101";   -- set sw <= X"fe", pushing btn(1)
 	sw(7 downto 0) <= X"fe";
-	
-  wait for 50 ns;
-	btn <= "011";	
+  wait for 1 us;
+  btn <= "011";	  -- push btn(2), and wait for finishing program
+  wait for 1 ms;
 
-  wait for 140000 ns;
-  btn <= "110";
+  -- ans = sqrt(X"c350") = X"df.9b"
+  -- hex[0, 1, 2, 3] = [83, 98, 0e, a1]
+  btn <= "110";   -- set sw <= X"50", pushing btn(0)
   sw(7 downto 0) <= X"50";
-
   wait for 50 ns;
-  btn <= "101";
+  btn <= "101"; -- set sw <= X"c3", pushing btn(1)
   sw(7 downto 0) <= X"c3";
+  wait for 1 us;
+  btn <= "011"; -- push btn(2), and wait for finishing program
+  wait for 1 ms;
 
-  wait for 50 ns;
-  btn <= "011";
-
-  wait for 70000 ns;
-WAIT;                                                        
-END PROCESS always;                                          
+WAIT;
+END PROCESS always;
 END sqrt_arch;

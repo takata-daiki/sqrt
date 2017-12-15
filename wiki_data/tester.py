05f3556a86a690e9abbf363a00a8ba06f28bd493
takata-daiki <takata-daiki@ed.tmu.ac.jp>
2017-12-15 13:50:10 +0900

    no message

--- wiki_data/tester.py
+++ wiki_data/tester.py
@@ -7,7 +7,7 @@ clear = True
 for i in range(1, 65535):
     ans = testpy.calc(i)
     large = math.sqrt(i)
-    small = large - 1./256
+    small = large - 1 / 256
     # over = large + 1./256
     if not (small <= ans and ans <= large):
         print("i=", i, ", small=", small, ", ans=", ans, ", large=", large)
